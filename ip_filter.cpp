8319207aa93490f405863a38b45da64b1c42ae5b
Filipp Gundelakh <f.gundelakh@yandex.ru>
2020-01-17 15:35:46 +0300

    Removed additional comments and output

--- ip_filter.cpp
+++ ip_filter.cpp
@@ -1,5 +1,3 @@
-#include <cassert>
-#include <cstdlib>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -7,12 +5,6 @@
 #include "ip4.h"
 #include "filter.h"
 
-// ("",  '.') -> [""]
-// ("11", '.') -> ["11"]
-// ("..", '.') -> ["", "", ""]
-// ("11.", '.') -> ["11", ""]
-// (".11", '.') -> ["", "11"]
-// ("11.22", '.') -> ["11", "22"]
 std::vector<std::string> split(const std::string &str, char d)
 {
     std::vector<std::string> r;
@@ -37,7 +29,7 @@ int main()
     try
     {
         std::vector<ip4> ip_pool;
-
+        // processing input
         for(std::string line; std::getline(std::cin, line);)
         {
             if (line.empty()) break;
@@ -45,51 +37,32 @@ int main()
             ip_pool.push_back({v[0]});
         }
 
-        // TODO reverse lexicographically sort
+        // reverse lexicographically sort
         std::sort(ip_pool.begin(),ip_pool.end(),std::greater<ip4>());
         for(auto &cur_ip : ip_pool)
         {
             std::cout << cur_ip <<std::endl;
         }
 
-        // 222.173.235.246
-        // 222.130.177.64
-        // 222.82.198.61
-        // ...
-        // 1.70.44.170
-        // 1.29.168.152
-        // 1.1.234.8
-
-        // TODO filter by first byte and output
+        // filter by first byte and output
         // ip = filter(1)
-        std::cout << "Filter 1:" <<std::endl;
         auto ip_1 = filter(ip_pool,[](ip4 ip){ return (ip>=ip4{1,0,0,0} && ip<ip4{2,0,0,0});});
         for(auto &cur_ip : ip_1)
         {
             std::cout << cur_ip <<std::endl;
         }
-        // 1.231.69.33
-        // 1.87.203.225
-        // 1.70.44.170
-        // 1.29.168.152
-        // 1.1.234.8
 
-        // TODO filter by first and second bytes and output
+
+        // filter by first and second bytes and output
         // ip = filter(46, 70)
-        std::cout << "Filter 46, 70:" <<std::endl;
         auto ip_46_70 = filter(ip_pool,[](ip4 ip){ return (ip>=ip4{46,70,0,0} && ip<ip4{46,71,0,0});});
         for(auto &cur_ip : ip_46_70)
         {
             std::cout << cur_ip <<std::endl;
         }
-        // 46.70.225.39
-        // 46.70.147.26
-        // 46.70.113.73
-        // 46.70.29.76
 
-        // TODO filter by any byte and output
+        // filter by any byte and output
         // ip = filter_any(46)
-        std::cout << "Filter 46 any:" <<std::endl;
         auto ip_any_46 = filter(ip_pool,[](ip4 ip) {
             unsigned char value = 46;
             bool hasValue = false;
@@ -109,40 +82,6 @@ int main()
         {
             std::cout << cur_ip <<std::endl;
         }
-        // 186.204.34.46
-        // 186.46.222.194
-        // 185.46.87.231
-        // 185.46.86.132
-        // 185.46.86.131
-        // 185.46.86.131
-        // 185.46.86.22
-        // 185.46.85.204
-        // 185.46.85.78
-        // 68.46.218.208
-        // 46.251.197.23
-        // 46.223.254.56
-        // 46.223.254.56
-        // 46.182.19.219
-        // 46.161.63.66
-        // 46.161.61.51
-        // 46.161.60.92
-        // 46.161.60.35
-        // 46.161.58.202
-        // 46.161.56.241
-        // 46.161.56.203
-        // 46.161.56.174
-        // 46.161.56.106
-        // 46.161.56.106
-        // 46.101.163.119
-        // 46.101.127.145
-        // 46.70.225.39
-        // 46.70.147.26
-        // 46.70.113.73
-        // 46.70.29.76
-        // 46.55.46.98
-        // 46.49.43.85
-        // 39.46.86.85
-        // 5.189.203.46
     }
     catch(const std::exception &e)
     {
